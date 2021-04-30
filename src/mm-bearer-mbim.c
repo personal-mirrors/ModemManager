0bf4f26bd51bc5cb77eae9e130340795b38c7383
Andrew Lassalle <andrewlassalle@chromium.org>
2021-04-30 15:32:26 -0700

    CHROMIUM: Disable multiplex on mbim.
    Until shill supports link multiplexing, disable it for mbim.
    
    BUG=b:186213732
    BUG=b:175305412
    TEST=test_that $DOOD suite:cellular_ota --fast
    TEST=Verify internet connection in dood when connecting and
    disconnecting multiple times. Disable and enable Mobile data without
    issues.
    
    Cq-Depend: chromium:2864011, chromium:2864012
    Change-Id: If794ad1e625f4227ce7abcebab1fa5597e296a57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/modemmanager-next/+/2864389
    Commit-Queue: Andrew Lassalle <andrewlassalle@chromium.org>
    Tested-by: Andrew Lassalle <andrewlassalle@chromium.org>
    Reviewed-by: Madhav . <madhavadas@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>

--- src/mm-bearer-mbim.c
+++ src/mm-bearer-mbim.c
@@ -1165,6 +1165,10 @@ _connect (MMBaseBearer        *self,
     const gchar              *data_port_driver;
     gboolean                  multiplex_supported = TRUE;
 
+    /* TODO(b/175305412): Reenable multiplex for mbim when shill supports it. */
+    multiplex_supported = FALSE;
+    mm_obj_dbg (self, "Force multiplex disabled on MBIM.");
+
     if (!peek_ports (self, &mbim, &data, callback, user_data))
         return;
 
