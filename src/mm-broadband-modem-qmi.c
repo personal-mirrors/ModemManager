e2409b19db1fdd489ee66a8718ea777977ab9b63
Aleksander Morgado <aleksandermj@chromium.org>
2023-01-10 10:17:42 +0000

    UPSTREAM: broadband-modem-qmi: ignore our own profile changed indications
    The device may emit a "WDS Profile Changed" indication triggered from
    our own "WDS Modify Profile", "WDS Create Profile" or "WDS Delete
    Profile" operations, so ensure those are fully ignored so that we
    don't emit the "Updated" signal in the ProfileManager interface.
    
    The logic keeps track of the amount of concurrent operations so that
    the signal is ignored for as long as there is at least one operation
    running.
    
    Fixes https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/issues/687
    
    (cherry picked from commit cc78a6439f71e3f58b97972d171b966a42476a98)
    
    BUG=b:264259856
    TEST=Manual testing in Trogdor with AT&T SIM card
    
    Cq-Depend: chromium:4152010
    Change-Id: I136efdb1a93f35138220ee68103d11215e76930a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/modemmanager-next/+/4155327
    Tested-by: Aleksander Morgado <aleksandermj@google.com>
    Reviewed-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Andrew Lassalle <andrewlassalle@chromium.org>
    Commit-Queue: Pavan Holla <pholla@google.com>

--- src/mm-broadband-modem-qmi.c
+++ src/mm-broadband-modem-qmi.c
@@ -185,6 +185,7 @@ struct _MMBroadbandModemQmiPrivate {
 
     /* WDS Profile changed notification ID (3gpp Profile Manager) */
     guint profile_changed_indication_id;
+    gint  profile_changed_indication_ignored;
 
     /* PS registration helpers when using NAS System Info and DSD
      * (not applicable when using NAS Serving System) */
@@ -6061,6 +6062,9 @@ modem_3gpp_profile_manager_list_profiles (MMIfaceModem3gppProfileManager  *self,
 /*****************************************************************************/
 /* Store profile (3GPP profile management interface) */
 
+static void profile_changed_indication_ignore (MMBroadbandModemQmi *self,
+                                               gboolean             ignore);
+
 typedef struct {
     QmiClientWds         *client;
     gint                  profile_id;
@@ -6116,6 +6120,7 @@ modify_profile_ready (QmiClientWds *client,
     g_autoptr(QmiMessageWdsModifyProfileOutput) output = NULL;
 
     self = g_task_get_source_object (task);
+    profile_changed_indication_ignore (self, FALSE);
 
     output = qmi_client_wds_modify_profile_finish (client, res, &error);
     if (!output) {
@@ -6156,8 +6161,9 @@ create_profile_ready (QmiClientWds *client,
     guint8               profile_index;
     g_autoptr(QmiMessageWdsCreateProfileOutput) output = NULL;
 
-    self = g_task_get_source_object (task);
     ctx = g_task_get_task_data (task);
+    self = g_task_get_source_object (task);
+    profile_changed_indication_ignore (self, FALSE);
 
     output = qmi_client_wds_create_profile_finish (client, res, &error);
     if (!output) {
@@ -6214,6 +6220,7 @@ store_profile_run (GTask *task)
         if (!self->priv->apn_type_not_supported)
             qmi_message_wds_create_profile_input_set_apn_type_mask (input, ctx->qmi_apn_type, NULL);
 
+        profile_changed_indication_ignore (self, TRUE);
         qmi_client_wds_create_profile (ctx->client,
                                        input,
                                        10,
@@ -6234,6 +6241,7 @@ store_profile_run (GTask *task)
         if (!self->priv->apn_type_not_supported)
             qmi_message_wds_modify_profile_input_set_apn_type_mask (input, ctx->qmi_apn_type, NULL);
 
+        profile_changed_indication_ignore (self, TRUE);
         qmi_client_wds_modify_profile (ctx->client,
                                        input,
                                        10,
@@ -6323,8 +6331,12 @@ delete_profile_ready (QmiClientWds *client,
                       GAsyncResult *res,
                       GTask        *task)
 {
-    GError *error = NULL;
+    MMBroadbandModemQmi *self;
+    GError              *error = NULL;
     g_autoptr(QmiMessageWdsDeleteProfileOutput) output = NULL;
+
+    self = g_task_get_source_object (task);
+    profile_changed_indication_ignore (self, FALSE);
 
     output = qmi_client_wds_delete_profile_finish (client, res, &error);
     if (!output || !qmi_message_wds_delete_profile_output_get_result (output, &error)) {
@@ -6364,6 +6376,7 @@ modem_3gpp_profile_manager_delete_profile (MMIfaceModem3gppProfileManager *self,
     input = qmi_message_wds_delete_profile_input_new ();
     qmi_message_wds_delete_profile_input_set_profile_identifier (input, QMI_WDS_PROFILE_TYPE_3GPP, profile_id, NULL);
 
+    profile_changed_indication_ignore (MM_BROADBAND_MODEM_QMI (self), TRUE);
     qmi_client_wds_delete_profile (QMI_CLIENT_WDS (client),
                                    input,
                                    10,
@@ -6392,8 +6405,36 @@ profile_changed_indication_received (QmiClientWds                         *clien
                                      QmiIndicationWdsProfileChangedOutput *output,
                                      MMBroadbandModemQmi                  *self)
 {
+    if (self->priv->profile_changed_indication_ignored > 0) {
+        mm_obj_dbg (self, "profile changed indication ignored");
+        return;
+    }
+
     mm_obj_dbg (self, "profile changed indication was received");
     mm_iface_modem_3gpp_profile_manager_updated (MM_IFACE_MODEM_3GPP_PROFILE_MANAGER (self));
+}
+
+static void
+profile_changed_indication_ignore (MMBroadbandModemQmi *self,
+                                   gboolean             ignore)
+{
+    /* Note: multiple concurrent profile create/update/deletes may be happening,
+     * so ensure the indication ignore logic applies as long as at least one
+     * operation is ongoing. */
+    if (ignore) {
+        g_assert_cmpint (self->priv->profile_changed_indication_ignored, >=, 0);
+        self->priv->profile_changed_indication_ignored++;
+        mm_obj_dbg (self, "ignoring profile update indications during our own operations (%d ongoing)",
+                    self->priv->profile_changed_indication_ignored);
+    } else {
+        g_assert_cmpint (self->priv->profile_changed_indication_ignored, >, 0);
+        self->priv->profile_changed_indication_ignored--;
+        if (self->priv->profile_changed_indication_ignored > 0)
+            mm_obj_dbg (self, "still ignoring profile update indications during our own operations (%d ongoing)",
+                        self->priv->profile_changed_indication_ignored);
+        else
+            mm_obj_dbg (self, "no longer ignoring profile update indications during our own operations");
+    }
 }
 
 /*****************************************************************************/
